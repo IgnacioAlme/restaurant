260d18e6377fdf46fc2f673aa4bff311e6723f7c
IgnacioAlme <ignacio.yair.almenar@gmail.com>
2021-07-19 18:16:32 -0300

    Rework javascript, splited into different files.

---
+++ src/website.js
@@ -0,0 +1,82 @@
+import loadHome from "./home";
+import loadMenu from "./menu";
+
+function createHeader() {
+    const header = document.createElement('header');
+    header.classList.add('header');
+
+    const restaurantName = document.createElement('h1');
+    restaurantName.classList.add('restaurant-name');
+    restaurantName.textContent = 'PizzaMac';
+
+    header.appendChild(restaurantName);
+    header.appendChild(createNav());
+    
+    return header;
+}
+
+function createNav() {
+    const nav = document.createElement('nav');
+
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('button-nav');
+    homeButton.textContent = "Inicio";
+    homeButton.addEventListener('click', (e) => {
+        if (e.target.classList.contains('active')) return;
+        setActiveButton(homeButton);
+        loadHome();
+    });
+
+
+    nav.appendChild(homeButton);
+    
+    return nav;
+}
+
+function setActiveButton(button) {
+    const buttons = document.querySelectorAll('.button-nav');
+
+    buttons.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove('active');
+        }
+    });
+    button.classList.add('active');
+}
+
+function createMain() {
+    const main = document.createElement('main');
+    main.classList.add('main');
+    main.setAttribute('id', 'main');
+    return main;
+}
+
+function createFooter() {
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+
+    const githubLink = document.createElement('a');
+    githubLink.href = 'https://github.com/IgnacioAlme';
+
+    const githubIcon = document.createElement('i');
+    githubIcon.classList.add('fab');
+    githubIcon.classList.add('fa-github');
+
+    githubLink.appendChild(githubIcon);
+    footer,appendChild(githubLink);
+
+    return footer;
+}
+
+function loadWebsite() {
+    const content = document.getElementById('content');
+
+    content.appendChild(CreateHeader());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
+
+    setActiveButton(document.querySelector('.button-nav'));
+    loadHome();
+}
+
+export default loadWebsite;
